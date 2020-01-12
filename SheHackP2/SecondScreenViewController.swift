0f5e65e0416782dd838e1a34e52aab00fedb061b
Emma Park <emma@alertlabs.com>
2020-01-12 02:30:38 -0500

    api calling

--- SheHackP2/SecondScreenViewController.swift
+++ SheHackP2/SecondScreenViewController.swift
@@ -11,39 +11,22 @@ import UIKit
 class SecondScreenViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
     var image: UIImage?
-    let image = UIImage(named: "image.png")
+    
    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let fileImage = image?.pngData()
+        let imageFile = UIImage(named: "IMG_8925")
+        let fileImage = imageFile!.pngData()
+        
+        //let fileImage = imageFile!
         // Do any additional setup after loading the view.
-        let semaphore = DispatchSemaphore (value: 0)
+        var semaphore = DispatchSemaphore (value: 0)
 
         let parameters = [
           [
-            "key": "apikey",
-            "value": "5f9324a8a588957",
-            "type": "text"
-          ],
-          [
-            "key": "iscreatesearchablepdf",
-            "value": "false",
-            "type": "text"
-          ],
-          [
-            "key": "issearchablepdfhidetextlayer",
-            "value": "false",
-            "type": "text"
-          ],
-          [
-            "key": "filetype",
-            "value": "jpg",
-            "type": "text"
-          ],
-          [
             "key": "file",
-            "src": "/Users/emmapark/Downloads/IMG_8925.JPG",
+            "src": "",
             "type": "file"
           ]] as [[String : Any]]
 
@@ -61,11 +44,8 @@ class SecondScreenViewController: UIViewController, UINavigationControllerDelega
               body += "\r\n\r\n\(paramValue)\r\n"
             } else {
               let paramSrc = param["src"] as! String
-              let fileData = fileImage
-                    // NSData(contentsOfFile:paramSrc, options:[]) as Data
-                if fileData != nil {
-                let fileContent = String(data: fileData, encoding: .utf8)!
-                    
+              let fileData = try! NSData(contentsOfFile:paramSrc, options:[]) as Data
+              let fileContent = String(data: fileData, encoding: .utf8)!
               body += "; filename=\"\(paramSrc)\"\r\n"
                 + "Content-Type: \"content-type header\"\r\n\r\n\(fileContent)\r\n"
             }
@@ -74,9 +54,9 @@ class SecondScreenViewController: UIViewController, UINavigationControllerDelega
         body += "--\(boundary)--\r\n";
         let postData = body.data(using: .utf8)
 
-        var request = URLRequest(url: URL(string: "https://api.ocr.space/parse/image")!,timeoutInterval: Double.infinity)
-        request.addValue("", forHTTPHeaderField: "")
-        request.addValue("multipart/form-data; boundary=--------------------------293326906587902853798281", forHTTPHeaderField: "Content-Type")
+        var request = URLRequest(url: URL(string: "https://api.taggun.io/api/receipt/v1/verbose/file")!,timeoutInterval: Double.infinity)
+        request.addValue("multipart/form-data", forHTTPHeaderField: "Content-Type")
+        request.addValue("7339315034e311ea8bfadfb7eb1aa8b5", forHTTPHeaderField: "apikey")
         request.addValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
 
         request.httpMethod = "POST"
@@ -94,7 +74,82 @@ class SecondScreenViewController: UIViewController, UINavigationControllerDelega
         task.resume()
         semaphore.wait()
 
-    }
+//        let semaphore = DispatchSemaphore (value: 0)
+//
+//        let parameters = [
+//          [
+//            "key": "apikey",
+//            "value": "5f9324a8a588957",
+//            "type": "text"
+//          ],
+//          [
+//            "key": "iscreatesearchablepdf",
+//            "value": "false",
+//            "type": "text"
+//          ],
+//          [
+//            "key": "issearchablepdfhidetextlayer",
+//            "value": "false",
+//            "type": "text"
+//          ],
+//          [
+//            "key": "filetype",
+//            "value": "jpg",
+//            "type": "text"
+//          ],
+//          [
+//            "key": "file",
+//            "src": "/Users/emmapark/Downloads/IMG_8925.JPG",
+//            "type": "file"
+//          ]] as [[String : Any]]
+//
+//        let boundary = "Boundary-\(UUID().uuidString)"
+//        var body = ""
+//        var error: Error? = nil
+//        for param in parameters {
+//          if param["disabled"] == nil {
+//            let paramName = param["key"]!
+//            body += "--\(boundary)\r\n"
+//            body += "Content-Disposition:form-data; name=\"\(paramName)\""
+//            let paramType = param["type"] as! String
+//            if paramType == "text" {
+//              let paramValue = param["value"] as! String
+//              body += "\r\n\r\n\(paramValue)\r\n"
+//            } else {
+//              let paramSrc = param["src"] as! String
+//              let fileData = fileImage!
+//                    // NSData(contentsOfFile:paramSrc, options:[]) as Data
+//                let fileContent = String(data: fileData, encoding: .utf8)!
+//
+//              body += "; filename=\"\(paramSrc)\"\r\n"
+//                + "Content-Type: \"content-type header\"\r\n\r\n\(fileContent)\r\n"
+//            }
+//          }
+//        }
+//        body += "--\(boundary)--\r\n";
+//        let postData = body.data(using: .utf8)
+//
+//        var request = URLRequest(url: URL(string: "https://api.ocr.space/parse/image")!,timeoutInterval: Double.infinity)
+//        request.addValue("", forHTTPHeaderField: "")
+//        request.addValue("multipart/form-data; boundary=--------------------------293326906587902853798281", forHTTPHeaderField: "Content-Type")
+//        request.addValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+//
+//        request.httpMethod = "POST"
+//        request.httpBody = postData
+//
+//        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+//          guard let data = data else {
+//            print(String(describing: error))
+//            return
+//          }
+//          print(String(data: data, encoding: .utf8)!)
+//          semaphore.signal()
+//        }
+//
+//        task.resume()
+//        semaphore.wait()
+//
+//    }
     
 
     /*
@@ -107,4 +162,5 @@ class SecondScreenViewController: UIViewController, UINavigationControllerDelega
     }
     */
 
+}
 }
